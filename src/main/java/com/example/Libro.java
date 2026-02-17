2f20711c06c9d7e4976fa1ccd806a6c0a5a40b5a
Joel <jmarinos@cesde.net>
2026-02-17 09:39:28 -0500

    Avance 2

--- src/main/java/com/example/Libro.java
+++ src/main/java/com/example/Libro.java
@@ -2,21 +2,29 @@ package com.example;
 
 public class Libro {
 
-
     private String titulo;
     private String autor;
     private int paginas;
 
-    public Libro(String titulo, String autor, int paginas) {
+    public Libro() {
         this.titulo = "desconocido";
         this.autor = "anonimo";
         this.paginas = 0;
     }
 
     public Libro(String titulo, String autor) {
-
+        this.titulo = titulo;
+        this.autor = autor;
         this.paginas = 0;
     }
+
+    public Libro(String titulo, String autor, int paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+    }
+
+   
 
     public void mostrarDetalles() {
         System.out.println("Libro: " + titulo +
